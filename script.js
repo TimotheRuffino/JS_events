bdb587d3b40c567544e8bdc860b1f224e336dfde
TimotheRuffino <timothe.ruffino@gmail.com>
2020-05-25 19:27:24 +0200

    final commit

--- script.js
+++ script.js
@@ -1,62 +1,94 @@
 
 // Fonctionnalité 1
 
-foot = document.querySelector('footer');
+let footElmt = document.querySelector('footer');
 
-let count = 0;
-count++
+let countElmt = 0;
+countElmt++
 
-foot.addEventListener('click', function() { 
-    console.log("clic numéro " + count++)
+footElmt.addEventListener('click', function() { 
+    console.log("clic numéro " + countElmt++)
 });
 
 // Fonctionnalité 2
 
-header = document.getElementById('navbarHeader'); 
-nav = document.getElementsByClassName('navbar-toggler');
+let headerElmt = document.getElementById('navbarHeader'); 
+let navElmt = document.getElementsByClassName('navbar-toggler');
 
-nav[0].addEventListener('click', function() {
-  header.classList.toggle("collapse")
+navElmt[0].addEventListener('click', function() {
+  headerElmt.classList.toggle("collapse")
 });
 
 //  Fonctionnalité 3
 
 
-edit = document.getElementsByClassName('card-body')[0].getElementsByClassName('btn-outline-secondary');
-text = document.querySelectorAll('p.card-text');
+let editElmt = document.getElementsByClassName('card-body')[0].getElementsByClassName('btn-outline-secondary');
+let textElmt = document.querySelectorAll('p.card-text');
 
-edit[0].addEventListener('click', function() {
-    text[0].style.color = "red"
+editElmt[0].addEventListener('click', function() {
+  textElmt[0].style.color = "red"
 });
 
 // Fonctionnalité 4
 
-edit_2 = document.getElementsByClassName('card-body')[1].getElementsByClassName('btn-outline-secondary');
+let edit_2Elmt = document.getElementsByClassName('card-body')[1].getElementsByClassName('btn-outline-secondary');
 
-edit_2[0].addEventListener('click', function() {
-  if (text[1].style.color === 'green') {
-    text[1].style.color = "";
+edit_2Elmt[0].addEventListener('click', function() {
+  if (textElmt[1].style.color === 'green') {
+    textElmt[1].style.color = "";
   }
   else {
-    text[1].style.color = 'green';
+    textElmt[1].style.color = 'green';
   }
     
 });
 
 // Fonctionnalité 5
 
-link = (document.getElementsByTagName('link')[0]);
-navbar = document.getElementsByTagName('header');
+let linkElmt = document.getElementsByTagName('link')[0];
+let navbarElmt = document.getElementsByTagName('header');
 
-navbar[0].addEventListener('dblclick', function() {
-  if (link.disabled == false) {
-    link.disabled = true;
+navbarElmt[0].addEventListener('dblclick', function() {
+  if (linkElmt.disabled === false) {
+    linkElmt.disabled = true;
   }
   else {
-    link.disabled = false;
+    linkElmt.disabled = false;
   }
 });
 
 // Fonctionnalité 6
+
+let btnViewElmt = document.querySelectorAll('.btn-success'); 
+let cardImgElmt = document.querySelectorAll('.card-img-top');
+
+ 
+for (let i = 0; i < btnViewElmt.length; i++) {
+  btnViewElmt[i].addEventListener('mouseover', function() {
+   
+      if (textElmt[i].style.display !== 'none'){
+        textElmt[i].style.display = 'none';
+        cardImgElmt[i].style.height = '20%';
+        cardImgElmt[i].style.width = '20%';
+      }
+    else {
+      textElmt[i].style.display = '';
+        cardImgElmt[i].style.height = '100%';
+        cardImgElmt[i].style.width = '100%';
+    }
+  }
+     )};
+
+
+// Fonctionnalité 7
+
+let cardElmt = document.querySelector('.album > .container > .row');
+let btnRightElmt = document.querySelector('.btn.btn-secondary.my-2');
+ 
+btnRightElmt.addEventListener('click', function() {
+  cardElmt.insertBefore(cardElmt.lastElementChild, cardElmt.firstElementChild);
+})
+
+// Fonctionnalité 8
 
 
